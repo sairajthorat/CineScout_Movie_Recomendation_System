f7aa99253f71edd0048202050b67714b15a62d79
Sairaj <thoratsairaj10@gmail.com>
2025-10-29 23:36:17 +0530

    fixed

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import logging
 
 logging.basicConfig(
     level=logging.INFO,
